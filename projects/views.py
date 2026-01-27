e4d65de9cdebff2c87633d845434ee479a39ea90
schiesscl <hansschiess@gmail.com>
2026-01-26 21:42:33 -0300

    Ajustes en modelo de proyectos

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from .models import Project
 from skills.models import Skill
 
 def home(request):
-    projects = Project.objects.all().order_by('-created_at')
+    projects = Project.objects.all().order_by('created_at')
     skills = Skill.objects.all().order_by('order')
     return render(request, 'home.html', {
         'projects': projects, 
