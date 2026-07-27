09ca416131c7050ef843680fb434011a035714d5
schiesscl <hansschiess@gmail.com>
2026-07-27 01:18:54 -0400

    fix(carousel): implement continuous sliding window of 3 items

--- projects/views.py
+++ projects/views.py
@@ -6,8 +6,16 @@ def home(request):
     projects = list(Project.objects.all().order_by('created_at'))
     skills = Skill.objects.all().order_by('order')
     
-    # Agrupar proyectos para carrusel responsivo
-    project_slides_desktop = [projects[i:i + 3] for i in range(0, len(projects), 3)]
+    n_projects = len(projects)
+    if n_projects > 3:
+        # Ventana deslizante continua: muestra 3 proyectos siempre (ej. 1,2,3 luego 2,3,4 etc.)
+        project_slides_desktop = [
+            [projects[(i + j) % n_projects] for j in range(3)]
+            for i in range(n_projects)
+        ]
+    else:
+        project_slides_desktop = [projects]
+
     project_slides_mobile = [[p] for p in projects]
     
     return render(request, 'home.html', {
